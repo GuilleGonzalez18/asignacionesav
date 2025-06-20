db82bc309b65790199b034b03db4a0239665c6b9
Guillermo Gonzalez <gugonzalez@geocom.com.uy>
2025-06-20 13:50:30 -0300

    Dandole permisos para acceder

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,9 @@ app.use(bodyParser.json());
 
 require('dotenv').config();
 
+const cors = require('cors');
+app.use(cors());
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
