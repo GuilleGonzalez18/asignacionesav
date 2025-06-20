7a51db405d80e836858e77531de0b1d81f1dc21d
Guillermo Gonzalez <gugonzalez@geocom.com.uy>
2025-06-20 13:45:19 -0300

    Fix ruta de index

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 
